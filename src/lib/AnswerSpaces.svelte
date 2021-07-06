2972aec9a0acaf6cef2bc0d6f588f77d2cb33ee1
Victor J. Bezak <victorjbezak@gmail.com>
2021-07-06 15:13:35 -0500

    updated GPT3 component and finished answer spaces algorithm

--- src/lib/AnswerSpaces.svelte
+++ src/lib/AnswerSpaces.svelte
@@ -1,13 +1,35 @@
 <script>
-	export let answer;
+import LetterBank from "./LetterBank.svelte";
+
+	export let answer = "";
+	let answerRows = 0;
+	let charsOnRow = 0;
+	let charsPerRow = [[]];
+
+	answer.split("").forEach((letter, i) => {
+		if (letter == " " && charsOnRow > 10) {
+			charsPerRow.push([]);
+			answerRows += 1;
+			charsOnRow = 0;
+		} else {
+			charsPerRow[answerRows].push(letter)
+			charsOnRow += 1;
+		}
+	})
+
+	console.log(charsPerRow);
 </script>
 
-<section id="answer-spaces" class="flex mt-6">
-	{#each answer as letter}
-		{#if letter == " "}
-			<div class="p-2 mx-1"></div>
-		{:else}
-			<div class="p-2 mx-1 border-b-2 border-black">{letter.toUpperCase()}</div>
-		{/if}
+<section id="answer-spaces" class="grid">
+	{#each charsPerRow as row}
+		<div class="flex my-3">
+			{#each row as letter}
+				{#if letter == " "}
+					<div class="p-2 mx-1"></div>
+				{:else}
+					<div class="p-2 mx-1 border-b-2 border-black">{letter.toUpperCase()}</div>
+				{/if}
+			{/each}
+		</div>
 	{/each}
 </section>
--- src/lib/OpenAi.svelte
+++ src/lib/OpenAi.svelte
@@ -5,32 +5,73 @@
   const API_KEY = 'sk-X7ZfhjrYtXiRqJZflLYZwcM9EqunebS9SWN5ZDGg';
   const openai = new OpenAi(API_KEY);
 
-  let theme;
+  let input;
+  let completionPrompt = `
+  The Human will provide a theme, and then the AI will provide a short phrase that belongs to that theme.
+
+  Human: halloween
+  AI: trick or treat
+
+  Human: halloween
+  AI:  ghosts are spooky
+
+  Human: the beach
+  AI: watch out for sharks
+
+  Human: the beach
+  AI: lets build a sand castle
+
+  Human: `;
+
   let promise;
 
   async function askBot(theme) {
     console.log(theme)
+    
     const gptResponse = await openai.complete({
       engine: 'davinci',
-      prompt: theme,
-      maxTokens: 5,
+      prompt: updatePrompt(theme),
+      maxTokens: 15,
       temperature: 0.9,
       topP: 1,
       presencePenalty: 0,
-      frequencyPenalty: 0,
-      bestOf: 1,
-      n: 1,
+      frequencyPenalty: 0.2,
+      bestOf: 3,
+      n: 3,
       stream: false,
-      stop: ['\n', "testing"]
+      stop: ['\n']
     })
 
+    console.log(gptResponse.data.choices)
     return gptResponse;
+  }
+
+  function updatePrompt(userTheme) {
+    return completionPrompt + `${userTheme}\n  AI: `;
   }
 
   function submit() {
     promise.then((val)=>{
-      answer = val.data.choices[0].text;
+      answer = bestChoice(val.data.choices);
+      // answer = val.data.choices[0].text;
+      console.log(answer)
     });
+  }
+
+  function bestChoice(choices) {
+    /* Remove special characters "_", ",", and "'" from text and trim extra whitespace.
+    Then return the longest of the api completion choices.
+    */
+    console.log("choices:", choices);
+
+    let cleanedChoices = choices.map(choice => choice.text.replaceAll(/\_|,|'/g, "").trim());
+    console.log(`cleanedChoices: ${cleanedChoices}`)
+    let longestChoice = cleanedChoices.reduce((carried, current) => {
+      return carried.length > current.length ? carried : current; 
+    });
+    console.log(`longestChoice: ${longestChoice}`);
+
+    return longestChoice;
   }
 </script>
 
@@ -39,9 +80,9 @@
   <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col w-full">
     <h2 class="text-gray-900 text-lg font-bold title-font mb-5">Enter a theme and get an AI generated prompt.</h2>
     <div class="relative mb-4">
-      <input bind:value={theme} type="text" id="theme" name="theme" placeholder="type theme here" class="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      <input bind:value={input} type="text" id="theme" name="theme" placeholder="type theme here" class="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
     </div>
-    <button on:click={() => promise = askBot(theme)} class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Generate Prompt</button>
+    <button on:click={() => promise = askBot(input)} class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Generate Prompt</button>
     <p class="text-xs text-gray-500 mt-3">*developer not responsible for AI generated results*</p>
   </div>
 
