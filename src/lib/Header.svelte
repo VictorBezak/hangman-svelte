e792394252ffcbdd5857bda97e9b4cc8aaa33a2d
Victor J. Bezak <victorjbezak@gmail.com>
2021-07-06 15:52:46 -0500

    added firebase hosting

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,9 +1,15 @@
 <script>
-  import logo from '../assets/male-solid.svg'
+  import { onMount } from 'svelte';
+  import logo from '../assets/male-solid.svg';
+
+  onMount(() => {
+    document.getElementById("logo")
+      .addEventListener("click", () => location.reload());
+  })
 </script>
 
 <header class="container absolute p-5 flex flex-col items-center md:flex-row">
-  <a href="/#" class="text-gray-700 flex items-center lg:w-1/5 lg:justify-center">
+  <a id="logo" href="/#" class="text-gray-700 flex items-center lg:w-1/5 lg:justify-center">
     <div class="flex items-end">
       <span class="text-xl">Hang</span>
       <img src={logo} alt="Hangman Logo" class="w-2 ml-1" />
